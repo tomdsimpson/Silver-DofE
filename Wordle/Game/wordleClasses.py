661dc5270f19e7c6b8948f7378952bbcb72a1d8e
Tom Simpson <tom@thesimpsonhouse.co.uk>
2022-03-27 21:40:47 +0100

    Adding basic menu

--- Wordle/Game/wordleClasses.py
+++ Wordle/Game/wordleClasses.py
@@ -27,25 +27,28 @@ class wordleUI:
         self.answer = self.answers[r.randint(0, len(self.answers)-1)]
         self.height = 1
 
+        # Troubleshoot
+        print(self.answer)
+
         # Initialising Tk stuff
         self.titleFont = ('Helvetica bold',40)
         self.labelFont = ('Helvetica bold',20)
 
-        master.geometry("500x600+1200+200")
-        master.config(background="#0000ff")
+        master.geometry("540x600+1200+200")
+        master.config(background="#eeeeee")
         master.title("Wordle")
-        master.iconphoto(False, tk.PhotoImage(file='./icon.png'))
+        #master.iconphoto(False, tk.PhotoImage(file='./icon.png'))
 
         frame = tk.Frame(master)
         frame.grid(row=1,column=1)
-        frame.config(background="#ff0000")
+        frame.config(background="#eeeeee")
 
         self.feedback = tk.Frame(master)
         self.feedback.grid(row=2,column=1)
-        self.feedback.config(background="#00ff00")
+        self.feedback.config(background="#eeeeee")
 
         # Making widgets
-        self.titleLabel = tk.Label(frame, width = 15, height = 1, text="Wordle", font=self.titleFont)
+        self.titleLabel = tk.Label(frame, width = 15, height = 1, text="Wordle", font=self.titleFont, background="#eeeeee")
         self.titleLabel.grid(row=1,column=1,columnspan=5)
 
         self.inputSection = tk.Text(frame, width = 15, height = 1, font=self.labelFont)
@@ -98,10 +101,45 @@ class wordleUI:
 
 
 
+# --- Utility Window --- #
+class utilityWindow:
+
+    def __init__(self, master, score):
+
+        self.root = ""
+
+        # Making widgets
+        self.frame = tk.Frame(master)
+        self.frame.pack()
+
+        #self.scoreLabel = tk.Label(self.frame, text=f"You made {score} guesses!")
+        #self.scoreLabel.pack()
+
+        self.retartButton = tk.Button(self.frame, text="Play", command=self.restartGame)
+        self.retartButton.pack(side=tk.LEFT)
+
+        self.quitButton = tk.Button(self.frame, text="Quit", command=self.quitGame)
+        self.quitButton.pack(side=tk.RIGHT)
+
+    def quitGame(self):
+        self.root.destroy()
+        util.destroy()
+    
+    def restartGame(self):
+
+        if self.root != "":
+            self.root.destroy()
+
+        self.root = tk.Tk()
+        wordle = wordleUI(self.root)
+        self.root.mainloop()
+
+
+
 # -- Main code --- #
-root = tk.Tk()
-wordle = wordleUI(root)
-root.mainloop()
+util = tk.Tk()
+utility = utilityWindow(util, 1)
+util.mainloop()
 
 
 
